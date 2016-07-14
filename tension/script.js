055c9573a7c3b74866482f8bfa9343bd6349d09a
veraxrl <vera.xu.iforc@gmail.com>
2016-07-15 00:21:45 +0300

    slightly polished

--- tension/script.js
+++ tension/script.js
@@ -1,9 +1,26 @@
 var app = angular.module('tensionApp', ["ngRoute","firebase"]); 
 
+// for ngRoute
+app.run(["$rootScope", "$location", function($rootScope, $location) {
+  $rootScope.$on("$routeChangeError", function(event, next, previous, error) {
+    // We can catch the error thrown when the $requireSignIn promise is rejected
+    // and redirect the user back to the home page
+    if (error === "AUTH_REQUIRED") {
+      $location.path("/login");
+    }
+  });
+}]);
+
+
 app.config(function($routeProvider) {
 	$routeProvider.when('/', {
 		controller: 'listCtrl',
-		templateUrl: 'templates/home.html'
+		templateUrl: 'templates/home.html',
+		resolve: {
+			 "currentAuth": function($firebaseAuth) {
+              	return $firebaseAuth().$requireSignIn();
+            }
+		}
 	})
 	$routeProvider.when('/channel/:channelId',{
 		controller:'channelCtrl',
@@ -11,15 +28,20 @@ app.config(function($routeProvider) {
 	})                                           
 	$routeProvider.when('/signup',{
 		controller:'signupCtrl',
-		templateUrl:'templates/signup.html'
+		templateUrl:'templates/signup.html',
+		resolve: {
+            "currentAuth": function($firebaseAuth) {
+                return $firebaseAuth().$waitForSignIn();
+            }
+        }
 	})
 	$routeProvider.when('/login',{
-		controller:'loginCtrl',
+		controller:'LoginCtrl',
 		templateUrl:'templates/login.html'
 	})
 });
 
-app.controller('listCtrl', function($scope, $http, $firebaseObject, $firebaseArray) {
+app.controller('listCtrl', function($scope, $http, $firebaseObject, $firebaseArray,$firebaseAuth) {
 	var ref = firebase.database().ref().child("msgs");
 	$scope.msgs= $firebaseArray(ref);
 
@@ -31,9 +53,15 @@ app.controller('listCtrl', function($scope, $http, $firebaseObject, $firebaseArr
 	    $scope.newMessage="";
   	};
 
+  	$scope.signout = function() {
+  		$scope.authObj = $firebaseAuth();
+  		$scope.authObj.$signOut();
+  		console.log("signed out");
+  	};
+
 });
 
-app.controller('channelCtrl', function($scope, $http, $firebaseObject, $firebaseArray,$routeParams) {
+app.controller('channelCtrl', function($scope, $firebaseObject, $firebaseArray, $routeParams) {
 	var ref = firebase.database().ref().child($routeParams.channelId).child("msgs");
 	$scope.msgs= $firebaseArray(ref);
 
@@ -76,29 +104,27 @@ app.controller('signupCtrl', function($scope, $http, $firebaseObject, $firebaseA
 
 });
 
-app.controller('loginCtrl', function($scope, $http, $firebaseObject, $firebaseArray,$routeParams,$firebaseAuth) {
-	$scope.authObj= $firebaseAuth();
-	$scope.signinUser = function() {
-		$scope.authObj.$signInWithEmailAndPassword($scope.email, $scope.password).then(function(firebaseUser) {
-	  		console.log("Signed in as:", firebaseUser.uid);
-		}).catch(function(error) {
-	  		console.error("Authentication failed:", error);
-		})
-	}
+app.controller('LoginCtrl', function($scope, $routeParams, $firebaseObject, $firebaseAuth) {
+    $scope.authObj = $firebaseAuth();
 
-	// $scope.authObj.$signInWithRedirect("google").then(function() {
- //  	// Never called because of page redirect
-	// }).catch(function(error) {
-	//   console.error("Authentication failed:", error);
-	// });
+    $scope.login = function() {
+        console.log($scope.email);
+        console.log($scope.password);
 
+        $scope.authObj.$signInWithEmailAndPassword($scope.email, $scope.password)
+        .then(function(firebaseUser) {
+            console.log("Signed in as:", firebaseUser.uid);
+            window.location.assign('http://localhost:8000/#/channel/General');
+
+        }).catch(function(error) {
+             console.error("Authentication failed:", error);
+        })
+
+    }
 });
-
 
 
 // $scope.channels.$loaded().then(function() {
 // 		$scope.channels.general= {name: "General", description:"general"};
 // 		$scope.channels.$save();
 // });
-
-
