3aee485517099a8218ed8f8ac5f1de560d00506e
veraxrl <vera.xu.iforc@gmail.com>
2016-07-14 14:34:25 +0300

    update with login

--- tension/script.js
+++ tension/script.js
@@ -8,6 +8,14 @@ app.config(function($routeProvider) {
 	$routeProvider.when('/channel/:channelId',{
 		controller:'channelCtrl',
 		templateUrl:'templates/general.html'
+	})                                           
+	$routeProvider.when('/signup',{
+		controller:'signupCtrl',
+		templateUrl:'templates/signup.html'
+	})
+	$routeProvider.when('/login',{
+		controller:'loginCtrl',
+		templateUrl:'templates/login.html'
 	})
 });
 
@@ -38,6 +46,55 @@ app.controller('channelCtrl', function($scope, $http, $firebaseObject, $firebase
   	};
 
 });
+
+app.controller('signupCtrl', function($scope, $http, $firebaseObject, $firebaseArray,$routeParams,$firebaseAuth) {
+
+	$scope.show = function() {
+		console.log($scope.name);
+		console.log($scope.email);
+		console.log($scope.password);
+	}
+
+	$scope.createUser = function() {
+		$scope.authObj = $firebaseAuth();
+		$scope.authObj.$createUserWithEmailAndPassword($scope.email,$scope.password)
+		.then(function(firebaseUser) {
+			//add user for printout:
+			var ref = firebase.database().ref().child("users").child(firebaseUser.uid);
+			$scope.user= $firebaseObject(ref);
+			$scope.user.$save().then(function(ref) {
+				ref.key == firebaseUser.uid;
+			}, function (error) {
+				console.log("Error:",error);
+			});
+		    console.log("User " + firebaseUser.uid + " created successfully!");
+		    console.log($scope.user);
+		    }).catch(function(error) {
+		    	console.error("Error: ", error);
+		  	})
+	}
+
+});
+
+app.controller('loginCtrl', function($scope, $http, $firebaseObject, $firebaseArray,$routeParams,$firebaseAuth) {
+	$scope.authObj= $firebaseAuth();
+	$scope.signinUser = function() {
+		$scope.authObj.$signInWithEmailAndPassword($scope.email, $scope.password).then(function(firebaseUser) {
+	  		console.log("Signed in as:", firebaseUser.uid);
+		}).catch(function(error) {
+	  		console.error("Authentication failed:", error);
+		})
+	}
+
+	// $scope.authObj.$signInWithRedirect("google").then(function() {
+ //  	// Never called because of page redirect
+	// }).catch(function(error) {
+	//   console.error("Authentication failed:", error);
+	// });
+
+});
+
+
 
 // $scope.channels.$loaded().then(function() {
 // 		$scope.channels.general= {name: "General", description:"general"};
